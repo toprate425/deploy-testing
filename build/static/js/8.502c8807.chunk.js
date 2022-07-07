42962a1c7a8182f76c07db4ad7b14e76e7209701
toprate425 <96052595+toprate425@users.noreply.github.com>
2022-07-06 18:59:18 -0700

    deploy

---
+++ build/static/js/8.502c8807.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[8],{87:function(t,e,p){"use strict";p.r(e);p(0),p(89);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=8.502c8807.chunk.js.map
